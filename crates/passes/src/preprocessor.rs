55d9ffba8630780b22d0e13d965315f7789c352e
presenthee <khyunhii@gmail.com>
2026-02-10 05:55:32 +0000

    Fix let ref processing

--- crates/passes/src/preprocessor.rs
+++ crates/passes/src/preprocessor.rs
@@ -451,7 +451,6 @@ impl mut_visit::MutVisitor for AstVisitor<'_> {
             && let LocalKind::Init(box e) = &mut local.kind
         {
             if matches!(mode, hir::BindingMode(hir::ByRef::Yes(_), _))
-                && matches!(e.kind, ExprKind::Unary(UnOp::Deref, _))
             {
                 self.let_ref_exprs.insert(id, e.clone());
                 self.lets_to_remove.insert(hir_stmt.hir_id);
